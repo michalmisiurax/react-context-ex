56b67d93e1e336afa03af0adb73d541fedcbeafb
michalmisiurax <michal.misiura.x@gmail.com>
2023-10-20 18:38:18 +0200

    init project

---
+++ src/components/Body/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode, useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import "./body.css";
+
+interface BodyProps {
+  children: ReactNode;
+}
+
+export const Body = ({ children }: BodyProps) => {
+  const { isDark } = useContext(AppContext);
+
+  const bodyClass = isDark ? "light" : "dark";
+
+  return <div className={`${bodyClass} bodyHeight`}>{children}</div>;
+};
